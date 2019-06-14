4c482c0946946ed39b02107dbe93c24cf6499609
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 17:54:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,21 @@ class Driver {
 
 class Route {
   constructor( beginningLocation, endingLocation ) {
-    this.beginningLocation = beginningLocation;
-    this.endingLocation = endingLocation;
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
   }
   avenueToInteger( avenue ) {
-    return eastWest.indexOf( avenue );
+    return eastWest.indexOf( avenue )
   }
 
+  blocksTravelled() {
+    let horizontalDistance =
+      this.avenueToInteger( this.endingLocation.horizontal ) -
+      this.avenueToInteger( this.beginningLocation.horizontal )
+    let verticalDistance =
+      this.endingLocation.vertical - this.beginningLocation.vertical
+    return Math.abs( horizontalDistance ) + Math.abs( verticalDistance )
+  }
+  
+  
 }
