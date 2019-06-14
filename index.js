529e6c9a92b17d1f918abade5a1c95f1437887b1
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 17:52:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+]
+
 class Driver {
   constructor( name, startDate ) {
-    this.name = name;
-    this.startDate = new Date( startDate );
+    this.name = name
+    this.startDate = new Date( startDate )
   }
 
 }
