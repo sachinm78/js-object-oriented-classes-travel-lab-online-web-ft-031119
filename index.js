88158c8c130a48b1e22fb3782d839f47a9d1e5e9
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 17:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ class Driver {
     this.startDate = new Date( startDate )
   }
 
+  yearsExperienceFromBeginningOf( year ) {
+    let endDate = new Date( year, 1, 1 )
+    let totalYears = ( endDate - this.startDate ) / ( 365 * 24 * 60 * 60 * 1000 )
+    return parseInt( totalYears )
+  }
 }
 
 class Route {
